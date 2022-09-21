d3d9bf1452e77fa2df44a0c3f0af025a5dc82072
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-09-20 21:45:16 -0500

    server file service

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ import { RouterResponse } from 'server/services/router/router.service';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
---
+++ server/services/file/file.service.ts
@@ -0,0 +1,65 @@
+import fs = require('fs');
+
+import server from 'server/server';
+
+export type FileService = {
+  exists: (filepath: string) => Promise<boolean>,
+  create: (filepath: string, content: string) => Promise<string>,
+  read: (filepath: string) => Promise<string>,
+  update: (filepath: string, content: string) => Promise<string>,
+  delete: (filepath: string) => Promise<string>,
+  move: (srcpath: string, destpath: string) => Promise<string>,
+  readDirectory: (path: string) => Promise<string[]>,
+  createDirectory: (path: string) => Promise<string>,
+  deleteDirectory: (path: string) => Promise<string>
+};
+
+export default {
+  exists: (filepath: string) => new Promise<boolean>(r=>fs.access(server.config.ROOT_DIR + filepath, fs.constants.F_OK, (e:any) => r(!e))),
+  create:  (filepath: string, content: string) => new Promise<string>((res, rej) => {
+    fs.writeFile(server.config.ROOT_DIR + filepath, content, (err: any) => {
+      if (err) { rej(err); } else { res('File created.'); }
+    });
+  }),
+  read:  (filepath: string) => new Promise<string>((res,rej) => {
+    fs.readFile(server.config.ROOT_DIR + filepath, "utf8", (err: any, file: string) => {
+      if (err) { rej(err); } else  {res(file); }
+    });
+  }),
+  update:  (filepath: string, content: string) => new Promise<string>((res, rej) => {
+    fs.appendFile(server.config.ROOT_DIR + filepath, content, (err: any) => {
+      if (err) { rej(err); } else { res('File updated.'); }
+    });
+  }),
+  delete:  (filepath: string) => new Promise<string>((res, rej) => {
+    fs.unlink(server.config.ROOT_DIR + filepath, (err: any) => {
+      if (err) { rej(err); } else { res('File deleted.'); }
+    });
+  }),
+  move:  (srcpath: string, destpath: string) => new Promise<string>((res, rej) => {
+    fs.rename(server.config.ROOT_DIR + srcpath, server.config.ROOT_DIR +  destpath, (err: any) => {
+      if (err) { rej(err); } else { res('File moved successfully.'); }
+    });
+  }),
+  readDirectory: (path: string): Promise<string[]> => {
+    return new Promise((res, rej) => {
+      fs.readdir(server.config.ROOT_DIR + path, (err: NodeJS.ErrnoException | null, files: string[]) => {
+        if (err) { rej(err); } else { res(files); }
+      });
+    });
+  },
+  createDirectory: (path: string): Promise<string> => {
+    return new Promise((res, rej) => {
+      fs.mkdir(server.config.ROOT_DIR + path, { recursive: true }, err => {
+        if (err) { rej(err); } else { res('Directory created successfully.'); }
+      });
+    });
+  },
+  deleteDirectory: (path: string): Promise<string> => {
+    return new Promise((res, rej) => {
+      fs.rmdir(server.config.ROOT_DIR + path, { recursive: true }, err => {
+        if (err) { rej(err); } else { res('Directory deleted successfully.'); }
+      });
+    });
+  }
+}
