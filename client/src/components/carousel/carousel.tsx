8c69783e2463149d2ddcce3856bbf4ae89e01366
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-10-15 21:06:23 -0500

    client carousel scroll buttons

--- client/src/components/carousel/carousel.tsx
+++ client/src/components/carousel/carousel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 import ProductCard from "../product-card/product-card";
 
@@ -8,19 +9,43 @@ export type CarouselProps = {
 }
 
 const Carousel: React.FC<CarouselProps> = (props: CarouselProps) => {
+
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  // const [scroll, setScroll] = React.useState<number>(0);
+
   return (
-    <div className="m-2">
+    <div className="relative m-2">
       <h2 className="inline-block">{props.categoryName}</h2>
-      <h2 className="inline-block float-right mr-1"><a href={props.categoryName} className="link link-primary">Browse All</a></h2>
-      <div className="carousel carousel-center rounded-box">
+      <h2 className="inline-block float-right mr-1"><Link to={"/browse/"+props.categoryName.replaceAll(' ', '')} className="link link-primary">Browse All</Link></h2>
+      
+      <div className="carousel carousel-center rounded-box" ref={ref}>
         {
           props.children.map((node, i) => (
-            <div className="carousel-item m-2" key={i}>
+            <div className="carousel-item m-2" key={i} id={props.categoryName.replaceAll(' ', '')+i}>
               { node }
             </div>
           ))
         }
       </div>
+
+      <a 
+        className="absolute z-10 btn btn-circle text-4xl left-5 top-1/2 p-8" 
+        // href={'#'+props.categoryName.replaceAll(' ', '')+((scroll + props.children.length-1) % props.children.length)}
+        // onClick={() => setScroll(s => (s + props.children.length-1) % props.children.length)}
+        onClick={()=>{ref.current!.scrollLeft -= .6*ref.current!.clientWidth; console.log(ref.current!.scrollLeft)}}
+      >
+        <span className="absolute -translate-y-1/2 top-1/2 -translate-x-1/6">❮</span>
+      </a>
+
+      <a 
+        className="absolute z-10 btn btn-circle text-4xl right-5 top-1/2 p-8"
+        // href={'#'+props.categoryName.replaceAll(' ', '')+((scroll + 1) % props.children.length)}
+        // onClick={() => setScroll(s => (s + 1) % props.children.length)}
+        onClick={()=>{ref.current!.scrollLeft += .6*ref.current!.clientWidth; console.log(ref.current!.scrollLeft)}}
+      >
+        <span className="absolute -translate-y-1/2 top-1/2 -translate-x-1/6">❯</span>
+      </a>
     </div>
   )
 }
