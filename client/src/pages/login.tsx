909791fe63f72ca33e4bb7be5b8f69725027b34e
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-10-23 16:47:43 -0500

    client login page functionality

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import HttpService from "../services/http.service";
 
 import ValidationService from "../services/validation.service";
 
 const Login: React.FC<any> = (props: any) => {
+
+  const [form, setForm] = React.useState<{email: string, password: string}>({email: "", password: ""});
+
+  const updateForm = (key: string, value: string) => setForm({ ...form, [key]: value });
+
+  const submit = () => {
+    HttpService.post('login', { email: form.email, password: form.password }).then(res => console.log(res)).catch(err => console.log(err));
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl mx-auto my-2">
       <div className="card-body">
@@ -14,16 +24,25 @@ const Login: React.FC<any> = (props: any) => {
           </label>
           <label className="input-group input-group-vertical">
             <span>Email</span>
-            <input type="text" placeholder="info@site.com" className="input input-bordered" />
+            <input type="text" placeholder="info@site.com" className="input input-bordered"
+               value={form.email}
+               onInput={(event: React.ChangeEvent<HTMLInputElement>) => updateForm('email', event.target.value)}
+            />
           </label>
           <label className="label">
             <span className="label-text">Your Password</span>
           </label>
           <label className="input-group input-group-vertical">
             <span>Password</span>
-            <input type="password" placeholder="password" className="input input-bordered" />
+            <input type="password" placeholder="password" className="input input-bordered" 
+              value={form.password}
+              onInput={(event: React.ChangeEvent<HTMLInputElement>) => updateForm('password', event.target.value)}
+            />
           </label>
-          <button className="btn btn-wide mx-auto mt-8">Submit</button>
+          <button className="btn btn-wide mx-auto mt-8"
+            disabled={!(form.email.length && form.password.length)}
+            onClick={submit}
+          >Submit</button>
         </div>
       </div>
     </div>
