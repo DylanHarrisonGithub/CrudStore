7f6494e22ec2a4bca46975f5be9b660772a8bee5
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-12-11 20:12:27 -0600

    client admin edit product modal

--- client/src/components/product-card/product-card.tsx
+++ client/src/components/product-card/product-card.tsx
@@ -1,15 +1,14 @@
 import React from "react";
+import config from "../../config/config";
 
-type CardType = {
-  src: string, alt: string, name: string, description: string
-}
+import { Product } from "../../models/models";
 
 export type ProductCardProps = {
   queryID?: string | undefined;
-  card?: CardType
+  product?: Product
 } & (
-  | { queryID: string; }
-  | { card: CardType }
+  | { queryID: string }
+  | { product: Product }
 );
 
 const api = '';
@@ -17,30 +16,48 @@ const api = '';
 // "https://placeimg.com/400/225/arch"
 const ProductCard: React.FC<ProductCardProps> = (props: ProductCardProps) => {
 
-  const [card, setCard] = React.useState<CardType>({ 
-    src: props.card?.src || "https://placeimg.com/250/180/arch", 
-    alt: props.card?.alt || "img source missing", 
-    description: props.card?.description || "Buy these shoes now! or else ...",
-    name: props.card?.name || "Product Name",
-  });
+  const [product, setProduct] = React.useState<Product>(
+    { 
+      id: props.product?.id || 0, 
+      name: props.product?.name || 'Coffee Maker', 
+      maker: props.product?.maker || 'DuPont', 
+      price: props.product?.price || 123, 
+      deal: props.product?.deal || 0,
+      description: props.product?.description || 'This thing makes coffee!', 
+      image: props.product?.image || "https://placeimg.com/400/220/arch?t=" + Math.floor(Math.random()*100000).toString(),
+      tags: props.product?.tags || 'coffee, appliance, beverages', 
+      stars: props.product?.stars || 3, 
+      reviews: props.product?.reviews || 0 
+    }
+  );
+    
+  //   { 
+  //   src: props.card?.src || "https://placeimg.com/250/180/arch", 
+  //   alt: props.card?.alt || "img source missing", 
+  //   description: props.card?.description || "Buy these shoes now! or else ...",
+  //   name: props.card?.name || "Product Name",
+  // });
   
   React.useEffect(() => {
-    if (props.queryID) {
-      // run db query for card by id
-      const fetchData = async () => {
-        const response = await fetch('');
-        setCard((await response.json()));
-      }
-      fetchData().catch(e => console.log(e));
+    // if (props.queryID) {
+    //   // run db query for card by id
+    //   const fetchData = async () => {
+    //     const response = await fetch('');
+    //     setProduct((await response.json()));
+    //   }
+    //   fetchData().catch(e => console.log(e));
+    // }
+    if (props.product) {
+      setProduct(props.product);
     }
-  }, []);
+  }, [props.product]);
 
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
-      <figure><img src={card.src} alt={card.alt} /></figure>
+      <figure><img src={config.ASSETS[config.ENVIRONMENT] + `products/${product.image}`} alt={"img source missing"} /></figure>
       <div className="card-body">
-        <h2 className="card-title">{card.name}</h2>
-        <p>{card.description}</p>
+        <h2 className="card-title">{`${product.maker} ${product.name}`}</h2>
+        <p>{product.description}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Add To Cart</button>
         </div>
---
+++ client/src/components/product-form/product-form.tsx
@@ -0,0 +1,198 @@
+import React, { useState } from 'react';
+
+import config from '../../config/config';
+
+import { Product } from '../../models/models';
+
+interface ProductProps {
+  product: Product,
+  productImageList: string[],
+  resolve: (product: Product) => any
+}
+
+const ProductForm: React.FC<ProductProps> = (props) => {
+
+  const [product, setProduct] = useState<Product>(props.product);
+
+  return (
+    <form className="w-full max-w-sm">
+
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="inline-product-image"
+          >
+            <img src={config.ASSETS[config.ENVIRONMENT] + `products/${product.image}`} className="w-32 h-32 float-right" />
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <select
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-product-image"
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setProduct(prev => ({ ...prev, image: e.target.value}))}
+            value={product.image}
+          >
+            {
+              props.productImageList.map(a => (<option key={a} value={a}>{a}</option>))
+            }
+          </select>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4">
+          <div>
+
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="inline-maker"
+                >
+                  Maker
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-maker"
+                  type="text"
+                  placeholder="Product Maker"
+                  value={product.maker}
+                  onInput={(event: React.ChangeEvent<HTMLInputElement>) => setProduct(prev => ({ ...prev, maker: event.target.value}))}
+                />
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="inline-name"
+                >
+                  Name
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-name"
+                  type="text"
+                  placeholder="guest"
+                  value={product.name}
+                  onInput={(event: React.ChangeEvent<HTMLInputElement>) => setProduct(prev => ({ ...prev, name: event.target.value}))}
+                />
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div>
+
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="inline-price"
+                >
+                  Price
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-price"
+                  type="text"
+                  placeholder="guest"
+                  value={product.price}
+                  onInput={(event: React.ChangeEvent<HTMLInputElement>) => setProduct(prev => ({ ...prev, price: parseFloat(event.target.value)}))}
+                />
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="inline-deal"
+                >
+                  Deal
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-deal"
+                  type="text"
+                  placeholder="guest"
+                  value={product.deal}
+                  onInput={(event: React.ChangeEvent<HTMLInputElement>) => setProduct(prev => ({ ...prev, deal: parseFloat(event.target.value)}))}
+                />
+              </div>
+            </div>
+
+
+          </div>
+      </div>  
+
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="inline-tags"
+          >
+            Tags
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-tags"
+            type="text"
+            placeholder="Product Maker"
+            value={product.tags}
+            onInput={(event: React.ChangeEvent<HTMLInputElement>) => setProduct(prev => ({ ...prev, tags: event.target.value}))}
+          />
+        </div>
+      </div>
+
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            htmlFor="inline-description"
+          >
+            Description
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <textarea
+            rows={2}
+            className="resize-none bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-description"
+            placeholder="Product Maker"
+            value={product.description}
+            onInput={(event: React.ChangeEvent<HTMLTextAreaElement>) => setProduct(prev => ({ ...prev, description: event.target.value}))}
+          />
+          
+        </div>
+      </div>   
+
+      <div className="container flex flex-col items-center">
+        <div className="md:w-1/3">
+          <button
+            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-5 mx-auto rounded"
+            type="button"
+            onClick={() => props.resolve(product)}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ProductForm;
